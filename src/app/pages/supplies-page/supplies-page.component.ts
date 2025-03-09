1f8f7cd8b41bdffb482e2bb788c22d1b60b1911a
Konstantin <vinnik-03@mail.ru>
2025-03-10 01:10:52 +0500

    feat: добавить sidebar, роутинг и карточки заявок
    - Реализован боковой навигационный sidebar
    - Настроен базовый роутинг между страницами
    - Добавлены компоненты для отображения карточек заявок
    
    version: 0.1.0

---
+++ src/app/pages/supplies-page/supplies-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supplies-page',
+  imports: [],
+  templateUrl: './supplies-page.component.html',
+  styleUrl: './supplies-page.component.scss'
+})
+export class SuppliesPageComponent {
+
+}
